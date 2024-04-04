ebe4c5d90fd491ae746ad2472c737709c9b838cc
JmJin <156559056+JasperJin01@users.noreply.github.com>
2024-04-05 00:55:39 +0800

    update240404

---
+++ boot-launch/src/main/java/com/jasper/bootlaunch/model/Article.java
@@ -0,0 +1,20 @@
+package com.jasper.bootlaunch.model;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Builder
+public class Article {
+    private Long  id;
+    private String author;
+    private String title;
+    private String content;
+    private Date createTime;
+
+    private List<Reader> reader;
+}
